aa0a727e68e2a746a6294871a754332c587a2fe7
unknown <fegoworks@hotmail.com>
2019-09-04 18:35:54 +0100

    feature(profile): collect user data as objects

---
+++ js/db.js
@@ -0,0 +1,71 @@
+let data = [{
+  name: "@fego",
+  cloudinary: "https://res.cloudinary.com/fego/image/upload/v1566565664/hng/fegggie_scbhf4.jpg",
+  message: "I contributed to payment API and team page ",
+  resume: "https://github.com",
+}, {
+  name: "@ruqayah",
+  cloudinary: "https://res.cloudinary.com/fego/image/upload/v1566565664/hng/fegggie_scbhf4.jpg",
+  message: "I was the boss lady",
+  resume: "https://gmail.com",
+}, {
+  name: "@ruqayah",
+  cloudinary: "https://res.cloudinary.com/fego/image/upload/v1566565664/hng/fegggie_scbhf4.jpg",
+  message: "I was the boss lady",
+  resume: "https://gmail.com",
+}, {
+  name: "@ruqayah",
+  cloudinary: "https://res.cloudinary.com/fego/image/upload/v1566565664/hng/fegggie_scbhf4.jpg",
+  message: "I was the boss lady",
+  resume: "https://gmail.com",
+}, {
+  name: "@ruqayah",
+  cloudinary: "https://res.cloudinary.com/fego/image/upload/v1566565664/hng/fegggie_scbhf4.jpg",
+  message: "I was the boss lady",
+  resume: "https://gmail.com",
+}, {
+  name: "@ruqayah",
+  cloudinary: "https://res.cloudinary.com/fego/image/upload/v1566565664/hng/fegggie_scbhf4.jpg",
+  message: "I was the boss lady",
+  resume: "https://gmail.com",
+}, {
+  name: "@ruqayah",
+  cloudinary: "https://res.cloudinary.com/fego/image/upload/v1566565664/hng/fegggie_scbhf4.jpg",
+  message: "I was the boss lady",
+  resume: "https://gmail.com",
+}, {
+  name: "@ruqayah",
+  cloudinary: "https://res.cloudinary.com/fego/image/upload/v1566565664/hng/fegggie_scbhf4.jpg",
+  message: "I was the boss lady",
+  resume: "https://gmail.com",
+}, {
+  name: "@ruqayah",
+  cloudinary: "https://res.cloudinary.com/fego/image/upload/v1566565664/hng/fegggie_scbhf4.jpg",
+  message: "I was the boss lady",
+  resume: "https://gmail.com",
+}, {
+  name: "@ruqayah",
+  cloudinary: "https://res.cloudinary.com/fego/image/upload/v1566565664/hng/fegggie_scbhf4.jpg",
+  message: "I was the boss lady",
+  resume: "https://gmail.com",
+}, {
+  name: " ",
+  cloudinary: " ",
+  message: " ",
+  resume: " ",
+}, {
+  name: " ",
+  cloudinary: " ",
+  message: " ",
+  resume: " ",
+}, {
+  name: " ",
+  cloudinary: " ",
+  message: " ",
+  resume: " ",
+}, {
+  name: " ",
+  cloudinary: " ",
+  message: " ",
+  resume: " ",
+}]
