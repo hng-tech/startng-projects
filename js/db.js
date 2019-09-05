f8c43ea002d8a2905c0fefd972d18a973e006c08
De-Bola <54313389+De-Bola@users.noreply.github.com>
2019-09-05 18:00:23 +0300

    re-populated the json file to make sure our team page is good

--- js/db.js
+++ js/db.js
@@ -1,31 +1,201 @@
 let data = [{
   name: "@ruqayaah",
-  cloudinary: "https://res.cloudinary.com/ruqayaah/image/upload/ar_16:9,c_fill,e_sharpen:116,g_auto,h_398,r_6,w_509/v1566635640/n3xpcvdrmrfaxjh6jt8t.jpg",
+  cloudinary: "https://res.cloudinary.com/ruqayaah/image/upload/w_400,h_400,c_scale/ar_16:9,c_fill,e_sharpen:116,g_auto,h_398,r_6,w_509/v1566635640/n3xpcvdrmrfaxjh6jt8t.jpg",
   message: "Team Lead",
   resume: "https://ruqayaahh.github.io/Resume/",
 }, {
   name: "@fego",
-  cloudinary: "https://res.cloudinary.com/fego/image/upload/v1566565664/hng/fegggie_scbhf4.jpg",
+  cloudinary: "https://res.cloudinary.com/fego/image/upload/w_400,h_400,c_scale/v1566565664/hng/fegggie_scbhf4.jpg",
   message: "Team Deputy. Worked on the payment page and team page ",
   resume: "https://fegoworks.github.io/startng-resume",
 }, {
   name: "@Bhummie",
-  cloudinary: "https://res.cloudinary.com/derbiet/image/upload/v1566584009/StartNg/imgomg_cbcvxa.jpg",
+  cloudinary: "https://res.cloudinary.com/derbiet/image/upload/w_400,h_400,c_scale/v1566584009/StartNg/imgomg_cbcvxa.jpg",
   message: "Github manager and Team deputy",
   resume: "https://sheltered-lowlands-78012.herokuapp.com",
 }, {
   name: "@Hermit",
-  cloudinary: "https://res.cloudinary.com/hng-pre-internship/image/upload/v1566498272/IMG_20160330_124834_lnrtem.jpg",
+  cloudinary: "https://res.cloudinary.com/hng-pre-internship/image/upload/w_400,h_400,c_scale/v1566498272/IMG_20160330_124834_lnrtem.jpg",
   message: "Built the testimonial section of the testimonials page and enhanced web-page interactivity.",
-  resume: " ",
+  resume: "https://tiberiusmairura.netlify.com/contact.html",
 }, {
   name: "@drey",
-  cloudinary: "https://res.cloudinary.com/drey/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1566498126/20190730_102625_qwqiez.jpg",
-  message: "Head of Ui/ux design team, i edited most of the team mockups, made contribution on front-end development, adding some lines of html and css codes",
+  cloudinary: "https://res.cloudinary.com/drey/image/upload/w_400,h_400,c_scale/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1566498126/20190730_102625_qwqiez.jpg",
+  message: " Team deputy.Head of Ui/ux design team, i edited most of the team mockups, made contributions on front-end development, adding some lines of html and css codes",
   resume: "https://drey771.github.io/",
 }, {
-  name: "",
-  cloudinary: "",
-  message: "",
-  resume: "",
+  name: "@Tobby",
+  cloudinary: " https://res.cloudinary.com/tobby/image/upload/w_400,h_400,c_scale/v1567343188/PhotoGrid_1546108321210.png",
+  message: "Deputy team lead, designed meet team section, re-designed header for all pages. ",
+  resume: "https://tobbyjay.github.io/Hng-Task2/index.html",
+},{
+  name: "@olaniyi900",
+  cloudinary: "https://res.cloudinary.com/olaniyi/image/upload/w_400,h_400,c_scale/v1566506556/20170804_133337_image_fkdtkk.jpg",
+  message: "I contributed to the front-end task by working on the landing page. ",
+  resume: "https://olaniyi900.github.io/hnginternship/",
+},{
+  name: "@Oluwadamilola ",
+  cloudinary: "https://res.cloudinary.com/damilolaesankonwea/image/upload/w_400,h_400,c_scale/v1566627563/IMG_20190819_141404_jcy3ba.jpg",
+  message: "I got almost all the pictures used for the project. I mean the group project. I sent them to @drey. So you can confirm ",
+  resume: "https://damilolaesankonwea.github.io/MyOriginalCV/",
+},{
+  name: "@Ebuka Harry",
+  cloudinary: "https://res.cloudinary.com/ebukaharrycloud/image/upload/w_400,h_400,c_scale/v1566562467/ebukaharry.jpg",
+  message: "Front-End, added Cloudinary links to the webpages.",
+  resume: "https://ebukaharry.github.io/ebukaharry-repo/",
+},{
+  name:  "@Sir Lighton",
+  cloudinary: "https://res.cloudinary.com/sir-lighton/image/upload/w_400,h_400,c_scale/v1566507311/IMG_20190813_133937_877_1_nsfncl.jpg",
+  message: "backend",
+  resume: "https://olaosebikan.github.io/OLAONIPEKUN",
+},{
+  name: "@Charles Ugbana",
+  cloudinary: "https://res.cloudinary.com/charlene04/image/upload/w_400,h_400,c_scale/v1567491197/B67I7267-Edit_dmu62i.jpg",
+  message: "Developed section4,5&6 of landing and section2 of ContactUs page.",
+  resume: "http://makeitpop.000webhostapp.com",
+},{
+  name: "@codyt",
+  cloudinary: "https://res.cloudinary.com/dmn1bwvq2/image/upload/w_400,h_400,c_scale/v1567499719/IMG_20190829_095855_3_2_ercsod.jpg",
+  message: "I conceptualized the UI homepage for Minds & Souls and also its structure also designed the team page UI.",
+  resume: "https://olanrewaju.netlify.com/",
+},{
+  name: "@Mide",
+  cloudinary: "https://res.cloudinary.com/mide358/image/upload/w_400,h_400,c_scale/v1566556773/IMG_20190820_110545_jxnkaw_rwv385.jpg",
+  message: "Front-end- I designed the About page (excluding the footer), I also collaborated with 5 people to design the contact page",
+  resume: "https://mide358.github.io/iyimide/",
+},{
+  name: "@Kponke",
+  cloudinary: "https://res.cloudinary.com/ekanem/image/upload/w_400,h_400,c_scale/v1566578868/start_ng/kponke_image.jpg",
+  message: "Page footer. Social icons. ",
+  resume: "https://k-ekanem.github.io",
+},{
+  name: "@Alex",
+  cloudinary: "https://res.cloudinary.com/pemualexandra/image/upload/w_400,h_400,c_scale/v1566641587/IMG_20190417_151731_1_qy85gh.jpg",
+  message: "I tested the  site and provided feedback and suggestions for improvement",
+  resume: "https://alex-pemu.github.io/StartNG-CV-TASK/",
+},{
+  name: "@siyfa",
+  cloudinary: "https://res.cloudinary.com/siyfa/image/upload/w_400,h_400,c_scale/e_art:incognito,fl_animated.any_format.attachment.clip.sanitize.strip_profile,r_14/v1566537499/dsiuwznszbcve3nzwmr0.jpg",
+  message: "Among the frontend team and contributed by also working on the homepage of the team website. Provided information and helping hand during the designing. ",
+  resume: "https://siyfa.github.io/Hakuna/",
+},{
+  name: "@Linda Adamu",
+  cloudinary: "https://res.cloudinary.com/draot9p1u/image/upload/w_400,h_400,c_scale/v1566621012/Linda.jpg",
+  message: "Back-End Developer",
+  resume: "https://lindyadam.github.io/web/",
+},{
+  name: "@peenarz",
+  cloudinary: "https://res.cloudinary.com/afaraitsulaimon/image/upload/w_400,h_400,c_scale/v1566558222/jnuxyo4qweq4vbfjkryi.jpg",
+  message: "Backend",
+  resume: "http://afaraitsulaimon.github.io/bolatask2",
+},{
+  name: "@codecayon",
+  cloudinary: "https://res.cloudinary.com/dzgbjty7c/image/upload/w_400,h_400,c_scale/v1566578851/IMG_20190806_174237_mldvgh.jpg",
+  message: "I bring in some ideas to the group which i think it helps in achieving our  goal.",
+  resume: "http://codecayon.github.io",
+},{
+  name: "@Tosin 'Lua",
+  cloudinary: "https://res.cloudinary.com/thecodingarchitect/image/upload/w_400,h_400,c_scale/v1566515502/DSC_95261_paauoi.jpg",
+  message: "Contact us page frontend and form validation",
+  resume: "https://thecodingarchi.github.io/",
+},{
+  name: "@Arinze",
+  cloudinary: "https://res.cloudinary.com/dzwtycmnl/image/upload/w_400,h_400,c_scale/v1566590092/IMG_2602_osa6ad.jpg",
+  message: "I contributed in working on the Contact Us page with @Tosin 'Lua and @Dolapo. M",
+  resume: "https://arinzecv.netlify.com/",
+},{
+  name: "@Joe Freely",
+  cloudinary: "https://res.cloudinary.com/lestrange/image/upload/w_400,h_400,c_scale/v1566509908/My%20Images/Joe_Freely_loal7c.jpg",
+  message: "I edited the section 5 of the website",
+  resume: "https://nsicache.github.io",
+},{
+  name: "@leela French",
+  cloudinary: "https://res.cloudinary.com/leelafrench/image/upload/w_400,h_400,c_scale/v1566658683/20171205_153516_qwd4rf.jpg",
+  message: "Discover bugs",
+  resume: "https://leelafrench.github.io/outcome1/Barileela.html",
+},{
+  name: "@Isreal Bankole",
+  cloudinary: "https://res.cloudinary.com/isrealbankole/image/upload/w_400,h_400,c_scale/v1567335233/28537503_813349345528547_1670912722_n_7_ihib8j.jpg",
+  message: "I helped in setting up the Meet The Team page and assisted in getting the images for the Home page ready and hosted on Cloudinary.",
+  resume: "https://oyefolarin.github.io/Start-NG-Projects/",
+},{
+  name: "@writeshittu",
+  cloudinary: "https://res.cloudinary.com/undercover/image/upload/w_400,h_400,c_scale/v1566576871/passport_zju0xb.jpg",
+  message: "Helped in writing codes on the volunteer section of the landing page. Also part of the team testing the responsiveness of the group project site",
+  resume: "https://shit2boy.github.io/hnginternship/",
+},{
+  name: "@anikriz",
+  cloudinary: "https://res.cloudinary.com/anikriz/image/upload/w_400,h_400,c_scale/v1566560640/HNG%20%7CStartng%20Internship/passport_photo-min_zremwg.jpg",
+  message: "I participated in the design of the home page, from the header to the about us section, and also designed a donation page for the team.",
+  resume: "https://anikriz.github.io/startng-training/",
+},{
+  name: "@Daniel Kolawole",
+  cloudinary: "https://res.cloudinary.com/djdlqcts6/image/upload/w_400,h_400,c_scale/v1566836724/20190826_085841_mdjjsl.jpg",
+  message: "I downloaded, and installed Getbarter app, as well as commented on the services the app provided on Google Play Store.",
+  resume: "https://dankola.github.io/hngtasks/",
+},{
+  name: "@Barbara Mangiri",
+  cloudinary: "https://res.cloudinary.com/barbzy-dev1/image/upload/w_400,h_400,c_scale/v1566506900/me_gltugm.jpg",
+  message: "I added the Landing Page",
+  resume: "http://portfolioforbarbara.tk/git-task1/",
+},{
+  name: "@helewud",
+  cloudinary: "https://res.cloudinary.com/dvag8e5pw/image/upload/w_400,h_400,c_scale/v1554309159/helewud.jpg",
+  message: "I worked with UI team, i contributed in drafting a mockup for the gallery page, volunteer page and testimonial page",
+  resume: "https://helewud.github.io/StartNG/",
+},{
+  name: "@the zoba",
+  cloudinary: "https://res.cloudinary.com/chizzy/image/upload/w_400,h_400,c_scale/v1567355207/IMG_20190825_081149_2.jpg",
+  message: "contributed to the responsiveness by adding some media queries and styles to the style sheet for smaller devices",
+  resume: "https://classychizzy.github.io/contact-form/",
+},{
+  name: "@AbbyJibril",
+  cloudinary: "https://res.cloudinary.com/abbyjibril/image/upload/w_400,h_400,c_scale/v1566573121/AbbyJibril_ftlsaj.jpg",
+  message: "I developed content for the Testimonials and Donate pages of the project. I also reviewed the pages and existing content for optimization.",
+  resume: "https://abbyjibril.github.io/",
+},{
+  name: "@meek",
+  cloudinary: "https://res.cloudinary.com/meekcreatives/image/upload/w_400,h_400,c_scale/ar_1:1,bo_5px_solid_rgb:ffffff,c_fill,g_auto,r_max,w_1000/v1566563359/Passport/Passport_W_-_Copy_fp3z5h.jpg",
+  message: "contributed in the ui designs of the team project",
+  resume: "https://mcbiare.github.io/",
+},{
+  name: "@Dolapo M.",
+  cloudinary: "https://res.cloudinary.com/dlapsie/image/upload/w_400,h_400,c_scale/v1567673532/samples/20190828_115715_jguybv.jpg",
+  message: "Contributed to the Contact us page  with Sub Grp 3  (Frontend) all under @hermit",
+  resume: "https://oluwadolamipo.github.io/"
+},{
+  name: "@Obinna",
+  cloudinary: "https://res.cloudinary.com/dusclsrqy/image/upload/w_400,h_400,c_scale/v1566517482/34053158_180572342605752_7005803408871391232_n.jpg_zfoqpt.jpg",
+  message: "I worked on the home page html and css and also added some content to the home page",
+  resume: "https://obinnadtq.github.io/HNG_tech/",
+},{
+  name: "@Esther Okafor",
+  cloudinary: "https://res.cloudinary.com/ddtilzrgl/image/upload/w_400,h_400,c_scale/v1566599316/Esther%20image/IMAG0310_1_hygj81.jpg",
+  message: "Led the SEO team and also gave contributions ",
+  resume: "https://codepen.io/estherokafor05/full/mdbqgoo",
+},{
+  name: "@Alex",
+  cloudinary: "https://res.cloudinary.com/pemualexandra/image/upload/w_400,h_400,c_scale/v1566641587/IMG_20190417_151731_1_qy85gh.jpg",
+  message: "I examined the website and provided feedback and support in the changes, I created a home button for the testimonials page. I created the email address and worked hand in hand with the social media team",
+  resume: "https://alex-pemu.github.io/StartNG",
+},{
+  name: "@Larrich",
+  cloudinary: "https://res.cloudinary.com/larrich/image/upload/w_400,h_400,c_scale/v1559471226/Larrich.jpg",
+  message: "I pitched designs for the homepage and Donate Now page.",
+  resume: "#"
+},{
+  name: "@Lanray",
+  cloudinary: "https://res.cloudinary.com/lanrayjay/image/upload/w_400,h_400,c_scale/v1567380184/HNG%20Resources/IMG_20180306_123902_resize_27_w6trxr.jpg",
+  message: "For now, not really defined but I've helped with constructive criticism of the minds and souls homepage and about (team) page; noting where work needs to be done, I'm working on the volunteer and contact form page for now.",
+  resume: "https://lanrayjay.github.io",
+},{
+  name: "@Dawillzy",
+  cloudinary: "https://res.cloudinary.com/dawillzy/image/upload/v1566511382/samples/IMG_20190801_182311_dvaeid.jpg",
+  message: "I'm a mobile dev, only gave some support from behind the scene.",
+  resume: "https://dawillzy.github.io/myResume",
+},{
+  name: "@isong josiah",
+  cloudinary: "https://res.cloudinary.com/isongjosiah/image/upload/w_400,h_400,c_scale/c_thumb,h_1135/v1566558716/20190308_131542_2_bpkovi.jpg",
+  message: "checked the website for bugs that needed to be fixed and called the attention of team members to the bugs.",
+  resume: "https://isongjosiah.github.io/Startdotng/"
 }]
