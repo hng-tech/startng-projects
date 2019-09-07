dfc836ca760a0687cba94e177dbed89178261a52
Bindas <bindas.fs@gmail.com>
2019-09-07 18:55:39 +0100

    removed php mailer

--- process.php
+++ process.php
@@ -1,8 +1,5 @@
 <?php
     session_start();
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
-    require 'vendor/autoload.php';
 
     if (isset($_POST['sub'])){
             
